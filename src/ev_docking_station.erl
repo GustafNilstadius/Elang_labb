3ad38bc7744bfdcf2b1a530e1ecc8d1f32f0c3a1
HiPERnx <gustaf.nilstadius@telia.com>
2016-02-14 19:22:19 +0100

    Added ev_docking_station

---
+++ src/ev_docking_station.erl
@@ -0,0 +1,12 @@
+%%%-------------------------------------------------------------------
+%%% @author Gustaf Nilstadius
+%%% @copyright (C) 2016
+%%% @doc
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+-module(ev_docking_station).
+-author("Gustaf Nilstadius").
+
+%% API
+-export([]).
