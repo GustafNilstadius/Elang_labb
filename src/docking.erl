29ff17fee238f47fd08a4c7691205db14cc56c63
HiPERnx <gustaf.nilstadius@telia.com>
2016-02-15 12:53:39 +0100

    Added docking_TEST.erl

--- src/docking.erl
+++ src/docking.erl
@@ -9,4 +9,26 @@
 -author("Gustaf Nilstadius").
 
 %% API
--export([]).
+-export([release/1, secure/1, init/2, get_info/1]).
+
+init(0, _) ->
+  {error,invalid_total};
+init(Total, Occupied) when Occupied < 0 ->
+  {error,invalid_occupied};
+init(Total, Occupied) when Occupied > Total ->
+  {error,invalid_occupied};
+init(Total, Occupied) ->
+  {ok, {Total, Occupied}}.
+
+release({Total, 0}) ->
+  {error, empty};
+release({Total, Occupied}) ->
+  {ok, {Total, Occupied-1}}.
+
+secure({Total, Total}) ->
+  {error, full};
+secure({Total, Occupied}) ->
+  {ok, {Total, Occupied+1}}.
+
+get_info({Total, Occupied}) ->
+  {ok, [{total, Total}, {occupied, Occupied}, {free, Total-Occupied}]}.
